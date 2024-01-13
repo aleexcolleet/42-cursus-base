c257dc11e8e22cb262c4c472a989df7d9bcb6844
Alex Collet Martínez <aleexcolleet@Alexs-MacBook-Pro.local>
2024-01-13 18:50:13 +0100

    vamos

--- 00-libft/libft/ft_memcpy.c
+++ 00-libft/libft/ft_memcpy.c
@@ -1,54 +1,22 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_memcpy.c                                        :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: acollet- <acollet-@student.42barcel>       +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/01/12 15:32:48 by acollet-          #+#    #+#             */
-/*   Updated: 2024/01/12 15:32:49 by acollet-         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "libft.h"
-/**************************************************************
-*						FT_MEMCPY:							  *
-*	La función ft_memcpy copia n bytes de una zona de memoria *
-*	origen (src) a una zona de memoria destino (dest).        *
-*	Devuelve un puntero a la zona de memoria destino.         *
-*															  *
-*	Argumentos:												  *
-*	dest: Un puntero a la zona de memoria destino donde se    *
-*	realizará la copia.										  *
-*	src: Un puntero a la zona de memoria origen desde donde   *
-*	se copiarán los datos.									  *
-*	n: El número de bytes que se copiarán desde src a dest.   *
-*															  *
-\*************************************************************/
-#include "libft.h"
-/*#include <stddef.h>*/
 
-void	*ft_memcpy(void *dest, const void *src, size_t len)
+void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
-	size_t	i;
+	unsigned char	*pdst;
+	unsigned char	*psrc;
+	size_t			i;
 
-	if (!dest && !src)
+	if (!dst && !src && n > 0)
 		return (NULL);
 	i = 0;
-	while (i < len)
+	pdst = (unsigned char *)dst;
+	psrc = (unsigned char *)src;
+	if (pdst == psrc)
+		return (pdst);
+	while (i < n)
 	{
-		((unsigned char *)dest)[i] = ((unsigned char *)src)[i];
+		pdst[i] = psrc[i];
 		i++;
 	}
-	return (dest);
+	return (dst);
 }
-/*memcpy is used to copy a block of memory from a location to another.
-int main(void)
-{
-    char dest[] = "ooooooo";
-    char src[] = "aaaaaaa";
-
-    printf("elmio----> %s", ft_memcpy(dest, src, 5));
-
-    return (0);
-}*/
