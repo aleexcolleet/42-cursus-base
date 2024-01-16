e820cbbdbc1afe2f8b516b806385d16af3dca6ee
Alex Collet Martínez <aleexcolleet@Alexs-MacBook-Pro.local>
2024-01-16 19:02:22 +0100

    vamoo

--- 00-libft/libft/ft_lstsize.c
+++ 00-libft/libft/ft_lstsize.c
@@ -2,43 +2,17 @@
 
 int	ft_lstsize(t_list *lst)
 {
-	int i;	
-	
+	int i;
+	t_list *d;
+
+	d = lst;
 	i = 0;
 	if (!lst)
 		return (0);
-	while (lst)
+	while(d)
 	{
-		lst = lst->next;
+		d = d->next;
 		i++;
 	}
 	return (i);
-}
-
-void	print_list(t_list *d)
-{
-	int a;
-
-	if (a == 1)
-		printf("la frase final--->");
-	while (d)
-	{
-		printf(" %s\t", (char *)d->content);
-		d = d->next;
-	}
-	a = 2;
-}
-int main(void)
-{
-	t_list *d;
-	d = ft_lstnew("hola");
-	d->next = ft_lstnew("como");
-	d->next->next = ft_lstnew("esta");
-	d->next->next->next = ft_lstnew("tuhermana");
-	print_list(d);
-	printf("\ncon %d nodos", ft_lstsize(d));
-	
-
-	free(d);
-	return (0);
 }
--- 00-libft/libft/libft.h
+++ 00-libft/libft/libft.h
@@ -61,4 +61,5 @@ char			*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 void			ft_putnbr_fd(int n, int fd);
 t_list			*ft_lstnew(void *content);
 void			ft_lstadd_front(t_list **lst, t_list *new);
+int 			ft_lstsize(t_list *lst);
 #endif
