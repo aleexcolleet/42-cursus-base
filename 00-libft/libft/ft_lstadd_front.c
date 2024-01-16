82dc55a3b7b4092a4058df360fd1528d9f98e367
aleexcolleet <aleexcolleet@gmail.com>
2024-01-16 15:46:18 +0100

    hola

--- 00-libft/libft/ft_lstadd_front.c
+++ 00-libft/libft/ft_lstadd_front.c
@@ -6,32 +6,41 @@
 /*   By: acollet- <acollet-@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/16 11:48:57 by acollet-          #+#    #+#             */
-/*   Updated: 2024/01/16 13:10:50 by acollet-         ###   ########.fr       */
+/*   Updated: 2024/01/16 15:46:05 by acollet-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
 
-void	ft_lstadd_front(t_list **lst, t_list *new)//el doble puntero nos permite mover *new. Asi como new nos permite cambiar le valor del nodo al que apunta. Asi como un array de strings, mas o menos.
+void	ft_lstadd_front(t_list **lst, t_list *new)
 {
-	if (!lst || !new)//why add something to an empty list
+	if (!lst || !new)
 		return ;
 	new->next = *lst;
 	*lst = new;
 }
 /*
-int	main(void)
+int main(void)
 {
-	t_list *list = ft_lstnew("como");
-	list->next = ft_lstnew("estamos");
+    t_list *lista = NULL;
 
-	t_list *node1 = ft_lstnew("hola");
-	ft_lstadd_front(&list, node1);
+    t_list *elemento1 = ft_lstnew("Elemento 1");
+    t_list *elemento2 = ft_lstnew("Elemento 2");
+    t_list *elemento3 = ft_lstnew("Elemento 3");
 
-	while (list != NULL)
+    ft_lstadd_front(&lista, elemento3);
+    ft_lstadd_front(&lista, elemento2);
+    ft_lstadd_front(&lista, elemento1);
+
+    t_list *current = lista;
+    while (current)
 	{
-		printf("%s\n", (char *)list->content);
-		list = list->next;
-	}
+        printf("%s\n", (char *)current->content);
+        current = current->next;
+    }
 
-	return (0);
+    free(elemento1);
+    free(elemento2);
+    free(elemento3);
+
+    return 0;
 }*/
