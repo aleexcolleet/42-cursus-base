e5e12cf0ec3824f11d05ad5223785ad9bef3eff5
aleexcolleet <aleexcolleet@gmail.com>
2024-01-17 12:08:45 +0100

    ahora

--- 00-libft/libft/ft_lstadd_back.c
+++ 00-libft/libft/ft_lstadd_back.c
@@ -6,7 +6,7 @@
 /*   By: acollet- <acollet-@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/17 10:42:45 by acollet-          #+#    #+#             */
-/*   Updated: 2024/01/17 11:11:08 by acollet-         ###   ########.fr       */
+/*   Updated: 2024/01/17 12:08:37 by acollet-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,29 @@
 
 void ft_lstadd_back(t_list **lst, t_list *new)
 {
-	
+	t_list *d;
+
+	if (!lst || !new)
+		return ;
+	d = *lst;
+	while (d)
+	{
+		d = d->next;
+	}
+	d = new;
+	d->next = 0;
 }
 int main(void)
 {
 	t_list *d = ft_lstnew("0");
-	d->next = ft_lstnew("1")
+	d->next = ft_lstnew("1");
+	t_list *s = ft_lstnew("2");
+	ft_lstadd_back(&d, s);
+	while (d)
+	{
+		printf("%s\t", (char *)d->content);
+	}
+	free(d);
+	free(s);
 	return (0);
 }
