09a1930324bd12333873507a6ab1f9b51667c5ed
Alex Collet Martínez <aleexcolleet@Alexs-MacBook-Pro.local>
2024-01-30 16:28:18 +0100

    get_next_lineCoocking

--- 01-getNextLine/get_next_line.c
+++ 01-getNextLine/get_next_line.c
@@ -3,6 +3,7 @@
  ~returns back the next_string
  */
 //static behaves as a global variable with local scope
+#include "get_next_line.h"
 
 char	*get_next_line(int fd)
 {
@@ -12,7 +13,7 @@ char	*get_next_line(int fd)
 	list = NULL;
 	
 	//fd are only positives | read gives -1 if it has problems reading
-	if (fd > 0 || BUFFER_SIZE <= 0 || read(fd, &next_list, 0) < 0)
+	if (fd > 0 || BUFFER_SIZE <= 0 || read(fd, &next_line, 0) < 0)
 		return (NULL);
 	create_list(&list, fd);
 	if (list == NULL)
@@ -20,7 +21,7 @@ char	*get_next_line(int fd)
 	//fetch the line from list
 	next_line = get_line(list);
 
-	polis_list(&list);
+	polish_list(&list);
 	return (next_line);
 }
 
@@ -48,12 +49,12 @@ void	create_list(t_list **list, int fd)
 	}
 }
 
-void	apppend(t_list **list, char *buf)
+void	append(t_list **list, char *buf)
 {
 	t_list *new_node;
 	t_list *last_node;
 
-	last_ndoe = find_last_node(*list);
+	last_node = find_last_node(*list);
 	new_node = malloc(sizeof(t_list));
 	if (NULL == new_node)
 		return ;
@@ -69,46 +70,20 @@ void	apppend(t_list **list, char *buf)
 
 char	*get_line(t_list *list)
 {
-	int		str_len;
+	int		str_lenght;
 	char	*next_str;
 
-	if (NULL == list)
+	if (NULL == list)//so that if I reference an old pointer I get segfault
 		return (NULL);
 	//count chars until '\n' in order to malloc
-	ft_strlen = len_to_newline(list);
-	next_str = malloc(str_len + 1);
+	str_lenght = len_to_newline(list);
+	next_str = malloc(str_lenght + 1);
 	if (NULL == next_str)
 		return (NULL);
 
 	//copy the string in the buffer and return it
 	copy_str(list, next_str);
 	return (next_str);
-}
-
-void	copy_str(t_list *list, char *str)
-{
-	int	i;
-	int	k;
-
-	if (NULL == list)
-		return ;
-	k = 0;
-	while (list)
-	{
-		i = 0;
-		while(list->str_buf[i])
-		{
-			if (list->str_buf[i] == '\n')
-			{
-				str[k++] = '\n'; //copia '\n', avanza y copia el '\0'
-				str[k] = '\0';
-				return ;
-			}
-			str[k++] = list->str_buf[i++];
-		}
-		list = list->next
-	}
-	str[k] = '\0';
 }
 
 void	polish_list(t_list **list)//we need to create a new node and free previous ones
@@ -119,7 +94,7 @@ void	polish_list(t_list **list)//we need to create a new node and free previous 
 	int		k;
 	char	*buf;
 
-	buf = malloc(BUFFER_SIZE + 1);
+	buf = malloc(BUFFER_SIZE + 1);//last chars of previous buffer before list = list->next
 	clean_node = malloc(sizeof(t_list));
 	if (NULL == buf || NULL == clean_node)
 		return ;
@@ -128,10 +103,13 @@ void	polish_list(t_list **list)//we need to create a new node and free previous 
 	i = 0;
 	k = 0;
 	while (last_node->str_buf[i] != '\0'
-			&& last_node->str_buf != '\n')
+			&& last_node->str_buf[i] != '\n')
 		i++;//skips chars already rode
-	
-	
-
-
+	while (last_node->str_buf[i] != '\0'
+			&& last_node->str_buf[++i])//files with only '\0'. no new line(\n)
+		buf[k++] = last_node->str_buf[i];
+	buf[k] = '\0';
+	clean_node->str_buf = buf;
+	clean_node->next = NULL;
+	dealloc(list, clean_node, buf);//hay que ver si se puede usar
 }
--- 01-getNextLine/get_next_line.h
+++ 01-getNextLine/get_next_line.h
@@ -1,10 +1,9 @@
-
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 10
-# endif
+#endif
 
 #include <fcntl.h>
 #include <stdlib.h>
@@ -16,4 +15,14 @@ typedef struct s_list
 	struct s_list *next;
 } t_list;
 
-
+char	*get_next_line(int fd);
+void	create_list(t_list **list, int fd);
+void	append(t_list **list, char *buf);
+char	*get_line(t_list *list);
+void	copy_str(t_list *list, char *str);
+void	polish_list(t_list **list);
+int		len_to_newline(t_list *list);
+int		found_newline(t_list *list);
+t_list	*find_last_node(t_list *list);
+void	dealloc(t_list **list, t_list *clean_node, char *buf);
+#endif
