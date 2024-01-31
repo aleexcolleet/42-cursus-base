be01509946547104be00a324cb5d0c990906d23b
Alex Collet Martínez <aleexcolleet@Alexs-MacBook-Pro.local>
2024-01-31 01:36:01 +0100

    porfin

---
+++ 01-ft_get_next_line/strings/get_next_line.h
@@ -0,0 +1,20 @@
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include <stdio.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+int		main(void);
+char	*get_next_line(int fd);
+void	ft_bzero(void *s, size_t n);
+size_t	ft_strlen(const char *str);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strjoin(char const *s1, char const *s2);
+
+#endif
---
+++ 01-ft_get_next_line/strings/get_next_line_utils.c
@@ -0,0 +1,65 @@
+#include "get_next_line.h"
+
+void	ft_bzero(void *s, size_t n)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < n)
+	{
+		*((unsigned char *)s + i) = '\0';
+		i++;
+	}
+}
+
+size_t	ft_strlen(const char *str)
+{
+	size_t	i;
+
+	if (str == NULL)
+		return (0);
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	if (s == NULL)
+		return (NULL);
+	while (*s != '\0')
+	{
+		if (*s == c)
+			return ((char *)s);
+		s++;
+	}
+	if (*s == '\0' && c == '\0')
+		return ((char *)s);
+	return (NULL);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*str;
+	size_t	len;
+	size_t	i;
+
+	len = ft_strlen(s1) + ft_strlen(s2) + 1;
+	str = (char *)malloc(len * sizeof(char));
+	if (!str)
+		return (NULL);
+	i = 0;
+	while (s1 && *s1)//el puntero s1 no es nulo y el caracter apuntado por s1 tmpoco
+	{
+		str[i++] = *s1;
+		s1++;
+	}
+	while (s2 && *s2)
+	{
+		str[i++] = *s2;
+		s2++;
+	}
+	str[i] = '\0';
+	return (str);
+}
