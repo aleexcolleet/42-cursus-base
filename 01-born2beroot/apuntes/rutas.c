0d215bf1cc174be6a993f680c05d189124412b64
Alex Collet Martínez <aleexcolleet@Alexs-MacBook-Pro.local>
2024-02-12 10:22:56 +0100

    adressBorn2beroot

---
+++ 01-born2beroot/apuntes/rutas.c
@@ -0,0 +1,12 @@
+Rutas:
+
+Interfaz grafica---> /usr/bin/*session
+
+Hostname ---> /etc/hostname y /etc/hosts
+
+Sudo configs---> /etc/sudoers.d/sudo_config
+Donde quedan comandos sudo---> /var/log/sudo/sudo_config
+
+Cronotab---> /etc/init.d/cron stop
+						/etc/init.d/cron start
+Password---> /etc/login.defs
